2f2b7d24c2576c59f7f80ac247cfc19d779bf51d
Sohan Deepak Preeti powar <84291806+sohan9619@users.noreply.github.com>
2021-06-24 22:00:07 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -46,10 +46,10 @@ contract Token {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public payable returns (bool success) {
-       // require(_value <= balanceOf[_from]);
+        require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
 
-       // balanceOf[_from] -= _value;
+        balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
 
         allowance[_from][msg.sender] -= _value;
